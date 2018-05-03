731e79295282f729b3a9ce22e79793762569c071
Andrew Jenkins <andrew.jenkins@centricient.com>
2018-05-03 15:03:39 -0600

    SER-7161

--- src/Utils/sentryLoglevelPlugin.js
+++ src/Utils/sentryLoglevelPlugin.js
@@ -27,13 +27,39 @@ const sentryPlugin = {
   },
 };
 
+const logIsBlacklisted = (message?: string, ex?: any) => {
+  try {
+    const blackList = ['post-robot', 'no handler found for post message'].map(m =>
+      m.replace(/[^\w]/gi, '').toLowerCase(),
+    );
+    const userMessage = (message ? message.toString().replace(/[^\w]/gi, '') : '').toLowerCase();
+    const exMessage = (ex && ex.message
+      ? ex.message.toString().replace(/[^\w]/gi, '')
+      : ''
+    ).toLowerCase();
+    const exStack = (ex && ex.stack
+      ? ex.stack
+          .replace(exMessage, '')
+          .toString()
+          .replace(/[^\w]/gi, '')
+      : ''
+    ).toLowerCase();
+
+    return blackList.some(
+      m => userMessage.includes(m) || exMessage.includes(m) || exStack.includes(m),
+    );
+  } catch (e) {
+    return false;
+  }
+};
+
 const errorLogger = (rawMethod, level, loggerName) => (
   message,
   {data = {}, exception = null} = {},
   shouldCapture = true,
 ) => {
   const extra = Object.assign({}, QuiqChatClient && QuiqChatClient._getState(), data);
-  if (shouldCapture) {
+  if (shouldCapture && !logIsBlacklisted(message, exception)) {
     if (exception) {
       Raven.captureException(exception, {
         level,
@@ -53,7 +79,7 @@ const errorLogger = (rawMethod, level, loggerName) => (
 
 const infoLogger = (rawMethod, level, loggerName) => (message, {data = {}, capture} = {}) => {
   // By default we only capture breadcrumbs for info level, but can be overridden for trace and debug.
-  if (capture || level === 'info') {
+  if ((capture || level === 'info') && !logIsBlacklisted(message)) {
     Raven.captureBreadcrumb({message, level, category: loggerName, data});
   }
   rawMethod(message);
