9b6a132673a124de5369d94d390a7425b57f2e63
Andrew Jenkins <andrew.jenkins@centricient.com>
2018-05-14 09:48:28 -0600

    1.55.3 - fix sentry blacklist

--- src/Utils/sentryLoglevelPlugin.js
+++ src/Utils/sentryLoglevelPlugin.js
@@ -27,45 +27,13 @@ const sentryPlugin = {
   },
 };
 
-/**
- * Any strings added to the blacklist below will not be logged to sentry if any of the following include the string
- * 1. Message logged to sentry
- * 2. Message in exception
- * 3. Stack trace in exception
- */
-const logIsBlacklisted = (message?: string, ex?: any) => {
-  try {
-    const blackList = ['post-robot', 'no handler found for post message'].map(m =>
-      m.replace(/[^\w]/gi, '').toLowerCase(),
-    );
-    const userMessage = (message ? message.toString().replace(/[^\w]/gi, '') : '').toLowerCase();
-    const exMessage = (ex && ex.message
-      ? ex.message.toString().replace(/[^\w]/gi, '')
-      : ''
-    ).toLowerCase();
-    const exStack = (ex && ex.stack
-      ? ex.stack
-          .replace(exMessage, '')
-          .toString()
-          .replace(/[^\w]/gi, '')
-      : ''
-    ).toLowerCase();
-
-    return blackList.some(
-      m => userMessage.includes(m) || exMessage.includes(m) || exStack.includes(m),
-    );
-  } catch (e) {
-    return false;
-  }
-};
-
 const errorLogger = (rawMethod, level, loggerName) => (
   message,
   {data = {}, exception = null} = {},
   shouldCapture = true,
 ) => {
   const extra = Object.assign({}, QuiqChatClient && QuiqChatClient._getState(), data);
-  if (shouldCapture && !logIsBlacklisted(message, exception)) {
+  if (shouldCapture) {
     if (exception) {
       Raven.captureException(exception, {
         level,
@@ -85,7 +53,7 @@ const errorLogger = (rawMethod, level, loggerName) => (
 
 const infoLogger = (rawMethod, level, loggerName) => (message, {data = {}, capture} = {}) => {
   // By default we only capture breadcrumbs for info level, but can be overridden for trace and debug.
-  if ((capture || level === 'info') && !logIsBlacklisted(message)) {
+  if (capture || level === 'info') {
     Raven.captureBreadcrumb({message, level, category: loggerName, data});
   }
   rawMethod(message);
--- src/sentry.js
+++ src/sentry.js
@@ -3,7 +3,7 @@ import {version} from '../package.json';
 import {inLocalDevelopment} from './Utils/utils';
 
 export const init = () => {
-  if (!inLocalDevelopment())
+  if (!inLocalDevelopment()) {
     Raven.config('https://5622397f17c44165a039d3b91d3e0193@sentry.io/170102', {
       release: version,
       autoBreadcrumbs: {
@@ -12,7 +12,24 @@ export const init = () => {
         dom: true, // DOM interactions, i.e. clicks/typing
         location: true, // url changes, including pushState/popState
       },
+      shouldSendCallback: data => {
+        try {
+          const blackList = [
+            'post-robot',
+            'no handler found for post message',
+            'window with name',
+          ].map(m => m.replace(/[^\w]/gi, '').toLowerCase());
+          const message = JSON.stringify(data)
+            .replace(/[^\w]/gi, '')
+            .toLowerCase();
+
+          return !blackList.some(m => message.includes(m));
+        } catch (e) {
+          return true;
+        }
+      },
     }).install();
+  }
 };
 
 export default init;
