90486f7aee4aad0c85518e90363dfed9b33e4a91
Fred Vollmer <fredvollmer@Freds-MacBook-Pro.local>
2018-11-01 10:46:39 -0600

    2.7.0 quiq-scripts 0.8.4

--- src/storage/__tests__/storage.test.ts
+++ src/storage/__tests__/storage.test.ts
@@ -5,22 +5,9 @@ jest.mock('../../State');
 import {isStorageEnabled} from '../index';
 
 describe('isStorageEnabled', () => {
-  it('fails if localStorage is disabled', () => {
-    // @ts-ignore
-    window.localStorage = {
-      getItem: () => {
-        throw new Error('getItem');
-      },
-      setItem: () => {
-        throw new Error('setItem');
-      },
-      clear: () => {
-        throw new Error('clear');
-      },
-      removeItem: () => {
-        throw new Error('removeItem');
-      },
-    };
+  fit('fails if localStorage is disabled', () => {
+    // @ts-ignore localStorage has been mocked globally
+    localStorage.setItem.mockImplementationOnce(() => { throw new Error('mocked disabled') });
 
     expect(isStorageEnabled()).toBe(false);
   });
